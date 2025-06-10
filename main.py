2148ee543a580c8001029c4b0c96deb9fff3112b
andre <andrechen@kellettschool.com>
2025-06-10 02:23:46 +0000

    Jun 10, 2025, 2:23 AM

--- main.py
+++ main.py
@@ -97,7 +97,7 @@ def Single_player(chosen_pokemon):
             print(f"You attacked! Rival's health is now {rival['health']}")
         elif action == "sleep":
             print("Why'd sleep you lazy bum?")
-        elif action == "gnaw" and "energy" > 0:
+        elif action == "gnaw":
             rival["health"] -= player["gnaw"]
             player["energy"] -= 1
             print(f"You attacked! Rival's health is now {rival['health']}")
